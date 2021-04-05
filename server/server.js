a9288f648318d90f66998e8ca68ca9066d0825b4
zweistein1326 <sid.agarwal45@gmail.com>
2021-04-05 18:42:48 +0800

    Server deployment

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+const app = express();
+const path = require('path')
+
+const publicPath = path.join(__dirname, '..', 'build')
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
