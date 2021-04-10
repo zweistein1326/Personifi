37341eded242af8e90fcbe8260c2280bb75e37b2
zweistein1326 <sid.agarwal45@gmail.com>
2021-04-11 02:10:03 +0800

    revise firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 
 var firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_API_KEY,
-    projectId: process.env.FIREBASE_API_KEY,
-    storageBucket:  process.env.FIREBASE_API_KEY,
-    messagingSenderId: "836595732873",
-    appId: "1:836595732873:web:b49fac44ccb6db90d4d158",
-    measurementId: "G-F3CCBH56VZ"
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_KEY
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
