a8d70998abd5045f819979ee0e50c1aef5e00c14
zweistein1326 <sid.agarwal45@gmail.com>
2021-04-11 01:38:04 +0800

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const app = express();
 const path = require('path')
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '..', 'build')
 
@@ -10,4 +11,4 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(port, () => console.log('Listening on port 3000'))
