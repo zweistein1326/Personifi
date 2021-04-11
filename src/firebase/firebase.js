d7200d8b7e8e0043cdac1ed8e9b1c9aa29884d51
zweistein1326 <sid.agarwal45@gmail.com>
2021-04-11 19:27:14 +0800

    Reset env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,14 @@
-import firebase from 'firebase/app';
-
-
-console.log(process.env.FIREBASE_PROJECT_ID);
-console.log(process.env.FIREBASE_API_KEY);
-console.log(process.env.FIREBASE_AUTH_DOMAIN);
-console.log(process.env.FIREBASE_STORAGE_BUCKET);
+import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_KEY
+    apiKey: `${process.env.FIREBASE_API_KEY}`,
+    authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+    projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.FIREBASE_APP_ID}`,
+    measurementId: `${process.env.FIREBASE_MEASUREMENT_KEY}`,
+    databaseUrl: `${process.env.FIREBASE_DATABASE_URL}`
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
